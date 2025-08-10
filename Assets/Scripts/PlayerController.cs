cce8481548de37ff51cc07c1267923454b4e7a24
ScarGrim <118862151+ScarGrim@users.noreply.github.com>
2025-08-10 16:10:10 -0500

    Update PlayerController.cs
    Add speed change capabilities

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private float movementX;
     private float movementY;
+    public float speed = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -31,7 +32,7 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(movementX, 0, movementY);
 
         //(movementVector) ? edit to movement
-        rb.AddForce(movement);
+        rb.AddForce(movement * speed);
    }
     // Update is called once per frame
     //void Update() {}
