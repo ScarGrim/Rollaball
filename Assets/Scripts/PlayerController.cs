f92d141f3ea089e14c27b43905246f1f4acc4891
ScarGrim <118862151+ScarGrim@users.noreply.github.com>
2025-08-10 15:37:53 -0500

    Materials + Move Scripts
    -Materials
    -PlayerControl Script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent <Rigidbody>();
+    }
+
+    void OnMove(InputValue movementvalue)
+    {
+        //tutorial
+        Vector2 movementVector = movementvalue.Get<Vector2>();
+
+        //added by function default
+        //Vector3 movement = new Vector3(inputVector.x, 0, inputVector.y);
+        //transform.Translate(movement * Time.deltaTime * 5f);
+
+        //tutorial
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+
+    }
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0, movementY);
+
+        //(movementVector) ? edit to movement
+        rb.AddForce(movement);
+   }
+    // Update is called once per frame
+    //void Update() {}
+}
